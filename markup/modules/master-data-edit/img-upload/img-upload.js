9abfc31248df82b347aec9202238dc076ecff655
graff <oldgraff@gmail.com>
2015-11-03 15:59:28 +0400

    wow

--- markup/modules/master-data-edit/img-upload/img-upload.js
+++ markup/modules/master-data-edit/img-upload/img-upload.js
@@ -1,31 +1,55 @@
 $(document).ready(function () {
-    $('.js-photo-upload').click(function () {
+    $('.various').click(function () {
         var selector = $(this).attr('href'); //передаем в переменную уникальный id из адресса ссылки
         initCropit(selector);                //Инициализация cropit и jquery-ui slider
         $(selector).find('.js-open-file').trigger('click'); //при клике на ссылке, происходит клик по лейблу с файл инпутом
-        if ($(selector).find('.cropit-image-background').width() != 0) {
-            $(selector).parents('.js-popup-photo-upload').first().css('display', 'table');
-        }
+        //if ($(selector).find('.cropit-image-background').width() != 0) {
+        //     $(selector).parents('.js-popup-photo-upload').first().css('display', 'table');
+        //}
         return false;
+    });
+    $('.js-photo-upload').click(function () {
+        $('#img-upload').find('.js-open-file').trigger('click');
     });
 });
 function initCropit(selector) {
+    var cropitSelector = selector + '-cropper';
     $(selector).find('.cropit-slider').slider({
         min: 0,
         max: 1,
         step: 0.01
     });
-    $(selector).cropit({
+    $(cropitSelector).cropit({
         imageBackground: true,
         onImageLoaded: function (object) {
-            $(selector).parents('.js-popup-photo-upload').first().css('display', 'table');
-            $(selector).find('.cropit-slider').slider('option', 'min', $(selector).cropit('zoom'));
-            $(selector).find('.cropit-slider').slider('option', 'value', $(selector).cropit('zoom'));
+            console.log($(cropitSelector).cropit('imageSize'));
+            //$(selector).parents('.js-popup-photo-upload').first().css('display', 'table');
+            //var bgImage = $(cropitSelector).find('.cropit-image-preview').css('background-image');
+            //console.log(bgImage.substr(3, bgImage.length - 1));
+            //$(cropitSelector).find('.cropit-image-background').attr('src', bgImage.substr(4, bgImage.length - 2));
+            if ($(selector).is(':hidden')) {
+                $.fancybox.open('#img-upload',
+                    {
+                        maxWidth: 800,
+                        maxHeight: 600,
+                        width: '70%',
+                        height: '70%',
+                        autoSize: false,
+                        closeEffect: 'none',
+                        afterClose: function () {
+                            $(selector).find('form').trigger('reset');
+                        }
+                    });
+            }
+            $(selector).find('.cropit-slider').slider('option', 'min', $(cropitSelector).cropit('zoom'));
+            $(selector).find('.cropit-slider').slider('option', 'value', $(cropitSelector).cropit('zoom'));
         }
     });
+
 }
 //Передаем значение value из jquery-ui slider в cropit для увел./умен. изображения
 $('.cropit-slider').on('slide', function (event, ui) {
+    $(this).parents('.image-cropper').first().cropit('zoom', ui.value);
     $(this).parents('.image-cropper').first().cropit('zoom', ui.value);
 });
 //Загрузка данных на сервер и вставка обрез. изображения в аватарку до загрузки на сервер
--- markup/modules/master-data-edit/master-data-edit.js
+++ markup/modules/master-data-edit/master-data-edit.js
@@ -2,16 +2,17 @@ $('.js-show-popup').click(function () {
     $(this).parents('div').first().find('.js-popup').css('display', 'table');
     return false;
 });
-$(document).ready(function () {
-    $('.various').fancybox({
-        maxWidth: 800,
-        maxHeight: 600,
-        fitToView: false,
-        width: '70%',
-        height: '70%',
-        autoSize: false,
-        closeClick: false,
-        openEffect: 'none',
-        closeEffect: 'none'
-    });
-});
+//$(document).ready(function () {
+//    $('#img-upload').fancybox({
+//        maxWidth: 800,
+//        maxHeight: 600,
+//        fitToView: false,
+//        width: '70%',
+//        height: '70%',
+//        autoSize: false,
+//        closeClick: false,
+//        openEffect: 'none',
+//        closeEffect: 'none'
+//    });
+//});
+
